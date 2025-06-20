01ea025970719bf099f6eb678bb303616f93de37
xavychin <xavychin@gmail.com>
2025-06-20 15:26:18 +0800

    test

---
+++ src/components/inputbox.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function inputbox() {
+  return (
+    <>
+      <label htmlFor="task_input"></label>
+      <input type="text" name="task_input"></input>
+    </>
+  );
+}
+
+export default inputbox;
---
+++ src/components/task_item.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function task_item() {
+  return (
+    <div>
+      <span>task</span>
+      <button>-</button>
+    </div>
+  );
+}
+
+export default task_item;
