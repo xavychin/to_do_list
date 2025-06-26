194212c0cecc661d158755edcb78c3df1db28c78
xavychin <xavychin@gmail.com>
2025-06-26 15:21:32 +0800

    test deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,4 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
+import { defineConfig } from "vite";
 export default defineConfig({
-  plugins: [react()],
-})
+  base: "/to_do_list/",
+});
